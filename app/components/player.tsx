962e9de81457c93bc611863ec5f660c9e2bcbe14
happydev0126 <jonehenry299@gmail.com>
2024-06-14 15:02:58 -0500

    Delete non created functions

--- app/components/player.tsx
+++ app/components/player.tsx
@@ -26,7 +26,7 @@ export default function Player({ className }: { className: string }) {
       </div>
       <div className="flex items-center justify-center gap-4">
         <>
-          <button className="btn-spotify" disabled onClick={() => { player?.shuffle() }} >
+          <button className="btn-spotify" disabled >
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" data-icon="SvgShuffle" aria-hidden="true"><path d="M21 18h-2.969a6 6 0 01-3.585-1.181M9.341 6.965a6.006 6.006 0 00-3.372-1.03H3m0 12.1h2.969A6.031 6.031 0 0012 12h0a6.031 6.031 0 016.031-6.031H21M18 9l3-3-3-3m.107 18.031l3-3-3-3"></path></svg>
           </button>
           <button className="btn-spotify" onClick={() => { player?.previousTrack() }} >
@@ -43,7 +43,7 @@ export default function Player({ className }: { className: string }) {
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" viewBox="0 0 24 24" data-icon="SvgSkipForward" aria-hidden="true"><path d="M14.4 12.524a1 1 0 000-1.548L9.85 7.25 5.983 4.086a1 1 0 00-1.633.774v13.78a1 1 0 001.633.774L9.85 16.25zm4.75-9.774v18"></path></svg>
           </button>
 
-          <button className="btn-spotify" disabled onClick={() => { player?.repeat() }} >
+          <button className="btn-spotify">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" viewBox="0 0 24 24" data-icon="SvgRepeat" aria-hidden="true"><path d="M17.251 3L21 6.752 17.251 10.5M21 6.759H5.805A2.807 2.807 0 003 9.566v1.628m3.751 2.35L3 17.3 6.7 21M3 17.289h15.2a2.807 2.807 0 002.8-2.808v-1.628"></path></svg>
           </button>
         </>
