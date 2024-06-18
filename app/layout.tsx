bf3cd4710bff9fe05df69f5191dad18ab619038e
happydev0126 <jonehenry299@gmail.com>
2024-06-17 22:27:13 -0500

    Fix no html tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,6 @@ export default async function RootLayout({
     token = await clerkClient.users.getUserOauthAccessToken(userId, provider).then(data => data.data[0].token)
     user = await getCurrentUser(token)
   }
-  if (!user) {
-    return
-  }
 
   return (
     <ClerkProvider >
@@ -39,22 +36,24 @@ export default async function RootLayout({
               </div>
             </SignedOut>
             <SignedIn>
-              <Providers token={token} user={user}>
-                <div className='grid h-screen w-full columns-auto grid-cols-[minmax(300px,400px),auto] grid-rows-[minmax(0,1fr)] gap-2 overflow-hidden bg-background p-2'>
-                  <Dashboard />
-                  <Card className='w-full'>
-                    <header className="flex justify-between">
-                      <div className="flex flex-row gap-4 text-gray-400">
-                        <button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" data-icon="SvgChevronLeft" aria-hidden="true"><path d="M14.9 6L12 9l-2.9 3 2.9 3 2.9 3"></path></svg></button>
-                        <button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" data-icon="SvgChevronRight" aria-hidden="true"><path d="M9.1 18l2.9-3 2.9-3L12 9 9.1 6"></path></svg></button>
-                      </div>
-                      <img src="/logos/01_RGB/02_PNG/Spotify_Logo_RGB_White.png" className='w-24' alt="Spotify_Logo_RGB_White" />
-                      <UserButton />
-                    </header>
-                    {children}
-                  </Card>
-                  <Player className='col-span-full' /> </div>
-              </Providers>
+              {user &&
+                <Providers token={token} user={user}>
+                  <div className='grid h-screen w-full columns-auto grid-cols-[minmax(300px,400px),auto] grid-rows-[minmax(0,1fr)] gap-2 overflow-hidden bg-background p-2'>
+                    <Dashboard />
+                    <Card className='w-full'>
+                      <header className="flex justify-between">
+                        <div className="flex flex-row gap-4 text-gray-400">
+                          <button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" data-icon="SvgChevronLeft" aria-hidden="true"><path d="M14.9 6L12 9l-2.9 3 2.9 3 2.9 3"></path></svg></button>
+                          <button><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" data-icon="SvgChevronRight" aria-hidden="true"><path d="M9.1 18l2.9-3 2.9-3L12 9 9.1 6"></path></svg></button>
+                        </div>
+                        <img src="/logos/01_RGB/02_PNG/Spotify_Logo_RGB_White.png" className='w-24' alt="Spotify_Logo_RGB_White" />
+                        <UserButton />
+                      </header>
+                      {children}
+                    </Card>
+                    <Player className='col-span-full' /> </div>
+                </Providers>
+              }
             </SignedIn>
           </main>
         </body>
