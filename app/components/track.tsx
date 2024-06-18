08e46bf147556ed01253cbcca7205cad2fd16b6d
happydev0126 <jonehenry299@gmail.com>
2024-06-18 00:54:02 -0500

    Revert "device id test"
    This reverts commit 268dd051cc2356ccb2e25f8a6ef0b5cb2f9a1b36.

--- app/components/track.tsx
+++ app/components/track.tsx
@@ -6,7 +6,6 @@ import { DeviceContext } from "../appContext"
 
 export default function Track({ item, index, token, playlist_uri }: { item: Item, index: number, token: string, playlist_uri: string }) {
   const { deviceId, user } = useContext(DeviceContext)
-  console.log({ deviceId })
 
   const formatTime = (trackDate: string) => {
     const date = new Date(trackDate)
@@ -36,7 +35,6 @@ export default function Track({ item, index, token, playlist_uri }: { item: Item
       return
     }
     if (deviceId) {
-      alert('you have device id')
       resumePlayback(token, deviceId, playlist_uri, index)
     }
   }
