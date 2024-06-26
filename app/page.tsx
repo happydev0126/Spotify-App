90a6775db28026897c8986bc93c679cf209093d4
happydev0126 <jonehenry299@gmail.com>
2024-06-26 14:56:29 -0500

    Fix: Duplicate tracks on recently played section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { auth, clerkClient } from "@clerk/nextjs/server";
 import { getFeaturedPlaylists, getRecentlyPlayed, getUsersTopItems } from "./api/spotify/spotify-api";
-import { Artist, FeaturedPlaylist, Item, Playlist } from "./types/spotify";
+import { Artist, FeaturedPlaylist, Item, Playlist, Track as SpotifyTrack } from "./types/spotify";
 import Link from "next/link";
 import Track from "./components/track";
 
@@ -20,31 +20,38 @@ export default async function Page() {
     featuredPlaylist = await getFeaturedPlaylists(token, 6).then(data => data.playlists)
   }
 
-  const playlistSet = new Set(recentlyPlayed)
-  playlistSet.forEach(playlist => console.log({ playlist }))
+
+  const removeDuplicates = (items: Item[]) => {
+    const seenItems = new Set()
+    return items.filter(item => {
+      const value = item.track.id
+      if (seenItems.has(value)) {
+        return false
+      }
+      seenItems.add(value)
+      return true
+    })
+  }
 
   // Get the Backend API User object when you need access to the user's information
 
   // Use `user` to render user details or create UI elements
   return (
     <div className="overflow-y-scroll overflow-x-hidden gap-8 flex flex-col">
-      {/* TODO */}
-      {/* <div className="row flex gap-2"> */}
-      {/*   <Tag title={'All'} /> */}
-      {/*   <Tag title={'Music'} /> */}
-      {/*   <Tag title={'Podcasts'} /> */}
-      {/* </div> */}
-      <section className="grid grid-cols-2 gap-4 xl:grid-cols-3">
-        {recentlyPlayed.map((item, index) => (
-          <Track key={item.track.id} item={item.track} index={index} token={token} uris={[item.track.uri]} />
-        ))}
+      <section className="flex flex-col gap-4 ">
+        <h3 className="text-2xl font-bold">Recently played tracks</h3>
+        <div className="grid grid-cols-2 gap-4 xl:grid-cols-3">
+          {removeDuplicates(recentlyPlayed).map((item, index) => (
+            <Track key={item.track.id} item={item.track} index={index} token={token} uris={[item.track.uri]} />
+          ))}
+        </div>
       </section>
       <section className="flex flex-col gap-4 ">
         <h3 className="text-2xl font-bold">Your favorite artists</h3>
         {(usersTopArtists.length < 1) ? <div className="text-xs italic">You dont have favorite artists yet</div> :
           <div style={{ scrollbarWidth: 'none' }} className="flex flex-row gap-4 overflow-x-scroll">
             {usersTopArtists.map((artist: Artist) => (
-              <Link href={`/artist/${artist.id}`} key={artist.id} className="flex flex-col items-start gap-2 p-2 hover:bg-gray-50/10">
+              <Link href={`/artist/${artist.id}`} key={artist.id} className="flex flex-col items-start gap-2 p-2 rounded hover:bg-gray-50/10">
                 <img src={artist.images[1].url} className="min-w-[11rem] max-w-[11rem] ratio aspect-square rounded" alt={artist.name} />
                 <div className="flex flex-col">
                   <p className="w-full font-bold text-sm">
@@ -63,7 +70,7 @@ export default async function Page() {
         <h3 className="text-2xl font-bold">Featured playlists</h3>
         <div style={{ scrollbarWidth: 'none' }} className="flex flex-row gap-4 w-full overflow-x-scroll">
           {featuredPlaylist.items.map((playlist: Playlist) => (
-            <Link href={`/playlist/${playlist.id}`} key={playlist.id} className="flex flex-col items-start gap-2 min-w-44 max-w-32 overflow-hidden">
+            <Link href={`/playlist/${playlist.id}`} key={playlist.id} className="flex flex-col items-start gap-2 p-2 rounded overflow-hidden hover:bg-gray-50/10">
               <img src={playlist.images[0].url} className="min-w-44 max-w-32 ratio aspect-square" alt={playlist.name} />
               <div className="flex flex-col">
                 <p className="w-full font-bold text-sm whitespace-nowrap text-ellipsis">
