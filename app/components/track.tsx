8c049b2d4485ca7851cb7cef8ca6a8e7ddf4e9e8
happydev0126 <jonehenry299@gmail.com>
2024-07-03 13:45:28 -0500

    Track button refactor

--- app/components/track.tsx
+++ app/components/track.tsx
@@ -7,8 +7,6 @@ import type { Track } from "../types/spotify"
 import { usePathname } from "next/navigation"
 import { convertMsToTimestamp } from "../lib/utils/convertMsToTimestamp"
 import { isoDateToMonthDayYear } from "../lib/utils/isoDateToMonthDayYear"
-import { currentUser } from "@clerk/nextjs/server"
-import { getToken } from "next-auth/jwt"
 
 export default function Track({ item, index, token, playlist_uri, uris, added_at }: { item: Track, index: number, token: string, playlist_uri?: string, uris?: string[], added_at?: string }) {
   const { deviceId, user } = useContext(DeviceContext)
@@ -37,6 +35,12 @@ export default function Track({ item, index, token, playlist_uri, uris, added_at
     return false
   }
 
+  const showPlay = isHover && !isCurrentlyPlaying(item.id) || isHover && is_active && is_paused
+  const showPause = (isHover && !is_paused) && isCurrentlyPlaying(item.id)
+  const showPlaying = (!isHover && isCurrentlyPlaying(item.id)) && !is_paused
+  const showActive = !isHover && isCurrentlyPlaying(item.id) && is_paused
+  const showNumber = (!showPlay && !showPause && !showPlaying && !showActive)
+
   /*   TODO
    *   Refactor this component
   */
@@ -51,28 +55,28 @@ export default function Track({ item, index, token, playlist_uri, uris, added_at
     >
       <div className="flex align-center justify-end w-full text-right min-w-6 text-sm">
         {
-          isHover && is_paused ?
-            <svg onClick={handlePlayTrack} className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-              <path fillRule="evenodd" d="M8.6 5.2A1 1 0 0 0 7 6v12a1 1 0 0 0 1.6.8l8-6a1 1 0 0 0 0-1.6l-8-6Z" clipRule="evenodd" />
-            </svg>
-            :
-            isHover && !is_paused && !isCurrentlyPlaying(item.id) ?
-              <svg onClick={handlePlayTrack} className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                <path fillRule="evenodd" d="M8.6 5.2A1 1 0 0 0 7 6v12a1 1 0 0 0 1.6.8l8-6a1 1 0 0 0 0-1.6l-8-6Z" clipRule="evenodd" />
-              </svg>
-              :
-              isHover && !is_paused && isCurrentlyPlaying(item.id) ?
-                <svg onClick={handlePauseTrack} className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                  <path fillRule="evenodd" d="M8 5a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2H8Zm7 0a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1Z" clipRule="evenodd" />
-                </svg> :
-                !isHover && isCurrentlyPlaying(item.id) && !is_paused ?
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="#1ED45E" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" data-icon="SvgActivity" aria-hidden="true"><path d="M3 12.3h3.3l.8-2.4.8-2.4 1.8 6.75L11.5 21l1.95-9 1.95-9 1 4.65 1 4.65H21"></path></svg>
-                  :
-                  !isHover && isCurrentlyPlaying(item.id) && is_paused ?
-                    <span className="text-green">{index + 1}</span> :
-                    isHover && !isCurrentlyPlaying(item.id) && is_active ?
-                      'hello' :
-                      index + 1
+          showPlay &&
+          <svg onClick={handlePlayTrack} className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            <path fillRule="evenodd" d="M8.6 5.2A1 1 0 0 0 7 6v12a1 1 0 0 0 1.6.8l8-6a1 1 0 0 0 0-1.6l-8-6Z" clipRule="evenodd" />
+          </svg>
+        }
+        {
+          showPause &&
+          <svg onClick={handlePauseTrack} className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            <path fillRule="evenodd" d="M8 5a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2H8Zm7 0a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1Z" clipRule="evenodd" />
+          </svg>
+        }
+        {
+          showPlaying &&
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" stroke="#1ED45E" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" data-icon="SvgActivity" aria-hidden="true"><path d="M3 12.3h3.3l.8-2.4.8-2.4 1.8 6.75L11.5 21l1.95-9 1.95-9 1 4.65 1 4.65H21"></path></svg>
+        }
+        {
+          showActive &&
+          <span className="text-green">{index + 1}</span>
+        }
+        {
+          showNumber &&
+          <span>{index + 1}</span>
         }
       </div>
       <div className="flex flex-row items-center gap-2">
