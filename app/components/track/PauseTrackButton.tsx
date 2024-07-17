2557e7cd2b602d18753d94ad3cf342241797a0de
happydev0126 <jonehenry299@gmail.com>
2024-07-17 18:41:57 -0500

    Show play and pause buttons on hover over albums and playlists

--- app/components/track/PauseTrackButton.tsx
+++ app/components/track/PauseTrackButton.tsx
@@ -5,7 +5,7 @@ import { pausePlayback } from "../../api/spotify/spotify-api"
 
 type Variant = 'green'
 
-export default function PauseTrackButton({ token, variant }: { token: string, variant: Variant }) {
+export default function PauseTrackButton({ token, variant }: { token: string, variant?: Variant }) {
   const { deviceId } = useContext(DeviceContext)
 
   const handlePauseTrack = (e: MouseEvent) => {
@@ -16,7 +16,7 @@ export default function PauseTrackButton({ token, variant }: { token: string, va
   }
 
   return (
-    <button className={variant === 'green' ? 'flex items-center justify-center w-8 h-8 absolute bg-green z-50 bottom-2 right-2 rounded-full text-black shadow' : ''} onClick={e => handlePauseTrack(e)}>
+    <button className={variant === 'green' ? 'flex items-center justify-center w-12 h-12 absolute bg-green z-50 bottom-2 right-2 rounded-full text-black shadow' : ''} onClick={e => handlePauseTrack(e)}>
       <img src="/icons/track/pause.svg" alt="Pause" />
     </button>
   )
